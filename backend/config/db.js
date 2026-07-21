2d408278d65c7967d2e7e1a2dbdbf1c56064c957
samarth15bose-code <samarth15bose@gmail.com>
2026-07-21 09:54:40 +0530

    Update db.js
    updated db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://samarth15bose_db_user:99edy6bINOYAgJ2Y@cluster0.qhbzoys.mongodb.net/food-del').then(()=>{
+    await mongoose.connect(process.env.MONGO_URI).then(() => {
         console.log("DB connected");
     })
 }
